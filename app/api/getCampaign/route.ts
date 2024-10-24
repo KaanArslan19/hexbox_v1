0af37a6b9d89b6781cd865d1bffa1df4ebce184c
Bradley Heubel <bradley.heubel@gmail.com>
2024-10-24 18:05:01 +1100

    Backend work
    Added createCampaign
    Added auth to api endpoints (currently commented out for createCampaign for ease of testing with postman).

--- app/api/getCampaign/route.ts
+++ app/api/getCampaign/route.ts
@@ -1,9 +1,17 @@
 import client from "@/app/utils/mongodb";
 import { NextRequest, NextResponse } from "next/server";
 import { ObjectId } from "mongodb";
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "@/app/utils/auth"
 
 export const GET = async (req: NextRequest, res: NextResponse) => {
     try {
+        const session = await getServerSession(authOptions)
+
+        if (!session) {
+            return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+        }
+
         console.log(req.nextUrl.searchParams);
         if (!req.nextUrl.searchParams.has("campaign")) {
             return NextResponse.json({ error: "Campaign ID is required" }, { status: 400 });
@@ -15,11 +23,11 @@ export const GET = async (req: NextRequest, res: NextResponse) => {
         }
         const mdbClient = client;
         const db = mdbClient.db("hexbox_main");
-        const movies = await db
+        const campaigns = await db
             .collection("campaigns")
             .find({ _id: new ObjectId(campaignId as string) })
             .toArray();
-        return NextResponse.json(movies);
+        return NextResponse.json(campaigns);
     } catch (e) {
         console.error(e);
         return NextResponse.json({ error: e }, { status: 500 });
