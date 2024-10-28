56b1900210977d3789f74e02b8ab02265e87d08c
KaanArslan19 <kaanarslan35@gmail.com>
2024-10-29 00:37:36 +1100

    mobile nav component has been cleansed

--- app/components/MobileNav.tsx
+++ app/components/MobileNav.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import { Collapse } from "@material-tailwind/react";
 import Link from "next/link";
 import { MenuItem } from "../types";
-import CustomButton from "./ui/CustomButton";
-import { ConnectButton } from "@rainbow-me/rainbowkit";
+
 import Wallet from "./Wallet";
 
 interface Props {
@@ -14,7 +13,7 @@ interface Props {
 export function MobileNav({ open, menuItems }: Props) {
   return (
     <>
-      <Collapse open={open} className=" my-2 ">
+      <Collapse open={open} className="  my-2 ">
         <ul className="space-y-4 border-y-2 border-lightBlueColor py-4 ">
           {menuItems.map(({ href, label }) => (
             <li key={href} className="text-lg mx-2 ">
@@ -26,8 +25,10 @@ export function MobileNav({ open, menuItems }: Props) {
             </li>
           ))}
         </ul>
-        <div className=" mt-2 text-center">
-          <Wallet />
+        <div className=" mt-2 text-center ">
+          <Link className="  px-4 py-1 " href="/">
+            <Wallet />
+          </Link>
         </div>
       </Collapse>
     </>
