a893f47375ac60b063d3ab08b097acc3b5bd2837
KaanArslan19 <kaanarslan35@gmail.com>
2024-10-29 00:33:01 +1100

    loading component created for build folder and next js version upgraded

--- app/(home_routes)/campaigns/page.tsx
+++ app/(home_routes)/campaigns/page.tsx
@@ -1,5 +1,5 @@
 import CampaignList from "@/app/components/campaign/CampaignList";
-import React, { Suspense } from "react";
+import React from "react";
 import CustomButton from "@/app/components/ui/CustomButton";
 import Link from "next/link";
 import { fetchCampaigns } from "@/app/utils/api";
@@ -8,29 +8,27 @@ export default async function CampaignsPage() {
   const campaigns = await fetchCampaigns();
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className="mx-auto max-w-2xl lg:max-w-6xl">
-        <div className="text-center mt-16 mx-2">
-          <div className="md:flex justify-between items-center mb-4">
-            <h1 className="text-4xl lg:text-5xl font-customFont_bold tracking-tight  mb-4 ">
-              Explore Campaigns
-            </h1>
-            <Link href="/create">
-              <CustomButton className="bg-none border-[1px] border-blueColor w-3/4 md:w-full">
-                Create a Campaign
-              </CustomButton>
-            </Link>
-          </div>
-
-          <p className="mx-auto text-lg lg:text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-            dolorem nobis quibusdam deserunt temporibus neque, quo et delectus
-            ullam quos, iste recusandae distinctio repellat! At labore excepturi
-            est saepe laudantium.
-          </p>
+    <div className="mx-auto max-w-2xl lg:max-w-6xl">
+      <div className="text-center mt-16 mx-2">
+        <div className="md:flex justify-between items-center mb-4">
+          <h1 className="text-4xl lg:text-5xl font-customFont_bold tracking-tight  mb-4 ">
+            Explore Campaigns
+          </h1>
+          <Link href="/create">
+            <CustomButton className="bg-none border-[1px] border-blueColor w-3/4 md:w-full">
+              Create a Campaign
+            </CustomButton>
+          </Link>
         </div>
-        <CampaignList listings={campaigns} />
+
+        <p className="mx-auto text-lg lg:text-xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+          dolorem nobis quibusdam deserunt temporibus neque, quo et delectus
+          ullam quos, iste recusandae distinctio repellat! At labore excepturi
+          est saepe laudantium.
+        </p>
       </div>
-    </Suspense>
+      <CampaignList listings={campaigns} />
+    </div>
   );
 }
