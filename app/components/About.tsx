fedb6cd3f1285001c4fd1cd8be0e9e9ee93342f5
KaanArslan19 <kaanarslan35@gmail.com>
2024-11-07 14:06:56 +1100

    HEXBOX-8 About Comp style improvement
    About component's mobile view changed.

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -24,7 +24,7 @@ export default function About() {
     },
   ];
   return (
-    <div className="flex flex-col items-center my-8  mx-auto">
+    <div className="flex flex-col items-center my-16  mx-auto">
       <h1 className="text-4xl xl:text-6xl capitalize text-center mb-4">
         Involve into Hexbox world <br />
         just in a Minute
@@ -33,26 +33,28 @@ export default function About() {
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus,
         pariatur magnam tempore veniam dolores incidunt recusandae voluptatem.
       </p>
-      <ul className="mt-4 flex flex-wrap gap-8 lg:flex-row lg:items-start items-center justify-center">
+      <ul className="mt-4 flex flex-wrap gap-8 xl:flex-row lg:items-start items-center justify-center">
         {ABOUT_DATA.map((item, index) => (
           <li
-            className="max-w-[24rem] h-[500px] flex flex-col overflow-hidden  bg-gradient-to-bl from-yellowColor/30 via-orangeColor/30 to-blueColor/30  rounded-2xl"
+            className="xl:max-w-[24rem] xl:h-[500px] min-h-[200px] flex xl:flex-col overflow-hidden  bg-gradient-to-bl from-yellowColor/30 via-orangeColor/30 to-blueColor/30  rounded-2xl"
             key={index}
           >
-            <div className="flex-1 flex items-center justify-center ">
+            <div className="xl:flex-1 flex items-center justify-center mx-2 ">
               <Image
-                className="h-[150px] w-[150px] rounded-full object-contain"
+                className="h-[150px] w-[150px] min-w-[100px] rounded-full  object-contain"
                 src={item.image}
                 alt={item.header}
                 width={100}
                 height={100}
               />
             </div>
-            <div className="flex-3 p-8">
-              <h4 className="text-blueColor  text-center text-2xl mb-2">
+            <div className="xl:flex-3 p-8 ">
+              <h4 className="text-blueColor  text-center  text-2xl mb-2">
                 {item.header}
               </h4>
-              <span className="mt-3">{item.description}</span>
+              <span className="mt-3 text-sm lg:text-base xl:text-lg">
+                {item.description}
+              </span>
             </div>
           </li>
         ))}
